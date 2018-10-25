f6d12f8422d44ca909f601cfb6f037eaa4782ad0
Guillaume Balas <guillaume.balas@gmail.com>
2018-10-24 21:14:08 -0500

    Rails Airbnb done with shit CSS

--- Gemfile
+++ Gemfile
@@ -60,3 +60,8 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'bootstrap-sass'
+gem 'font-awesome-sass', '~> 5.0.9'
+gem 'simple_form'
+gem 'autoprefixer-rails'
+gem 'jquery-rails' # Add this line if you use Rails 5.1 or higher
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Cosy & Modern Flat Paris',
+  address: '54 Buttes Chaumont, 750010 Paris',
+  description: 'A typical charming "Amélie" flat. 1 double bedrooms, open plan living area, kitchenette',
+  price_per_night: 100,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat Paris',
+  address: '17 Rue Molitor, 75016 Paris',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: 'Beautifil Country House Santiago',
+  address: 'El Atardecer 4021, Lo Barnechea, Santiago',
+  description: 'A modern spacious country house, 15 minutes away from Santiago downtwon. 4 double bedrooms, 2 single bedrooms, open plan living area, large kitchen, movie theather, pool',
+  price_per_night: 75,
+  number_of_guests: 12
+)
